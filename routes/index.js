f91f36b92a7853fca5ff9334086d54a11e86a0c0
JacquesOP <jacquesoprince.dev@gmail.com>
2024-03-07 22:31:02 +0100

    backend

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,10 @@ const router = express.Router();
 /*
 ============ Import modules ============ 
 */
+require("../models/connection");
+const User = require("../models/users");
+
+
 const uniqid = require("uniqid");
 const cloudinary = require("cloudinary").v2;
 const fs = require("fs");
@@ -23,25 +27,28 @@ router.get("/", function (req, res, next) {
 
 
 /*============ POST route to handle PDF file upload ============ */
-router.post("/upload", async(req, res) => {
+router.post("/upload/:token/:document", async(req, res) => {
+	
+	const token = req.params.token;
+	const document = req.params.document;
+	
+	console.log(token);
+	console.log(document);
 	
 	const file = req.files.pdfFile;
 	const username = file.name + '-';
 
 	const documentPath = `./tmp/${uniqid()}.pdf`;
-
 	const resultMove = await req.files.pdfFile.mv(documentPath);
-	
-	console.log(file);
-	console.log(username);
 
 	if(!resultMove){
       const resultCloudinary = await cloudinary.uploader.upload(documentPath, { 
 			folder: `NOVATERIM/users/${username}`,
-			public_id: `${uniqid()}`
+			public_id: `${uniqid()}_pdf`
 		});
 		console.log(resultCloudinary);
       res.json({ result: true, url: resultCloudinary.secure_url, name: resultCloudinary.original_filename });
+		User.updateOne({ })
    }else{
       res.json({ result: false, error: resultMove });
    }
