7eb53b1382b496d804d34fb56e23dff24b88297e
JacquesOP <jacquesoprince.dev@gmail.com>
2024-03-07 13:27:36 +0100

    backend

--- routes/index.js
+++ routes/index.js
@@ -6,29 +6,12 @@ const router = express.Router();
 /*
 ============ Import modules ============ 
 */
-const multer = require('multer');
-const path = require('path');
-
 const uniqid = require("uniqid");
 const cloudinary = require("cloudinary").v2;
 const fs = require("fs");
 
 const cloudinary_KEY = process.env.CLOUDINARY_URL;
 
-
-// Setting up Multer for file upload
-const storage = multer.diskStorage({
-	destination: function (req, file, cb) {
-		cb(null, 'uploads/') // Destination folder for uploads
-	},
-	filename: function (req, file, cb) {
-
-		// Using the original file name
-		cb(null, file.originalname);
-	}
-});
-
-const upload = multer({ storage: storage });
 
 
 
@@ -39,31 +22,31 @@ router.get("/", function (req, res, next) {
 
 
 
-/*============ POST route to handle file upload ============ */
-router.post("/upload", upload.single('pdfFile'), async(req, res) => {
+/*============ POST route to handle PDF file upload ============ */
+router.post("/upload", async(req, res) => {
 	
 	const file = req.files.pdfFile;
+	const username = file.name + '-';
+
+	const documentPath = `./tmp/${uniqid()}.pdf`;
+
+	const resultMove = await req.files.pdfFile.mv(documentPath);
+	
 	console.log(file);
+	console.log(username);
 
-	try {
-		const result = await cloudinary.uploader.upload(file.tempFilePath, 
-			{ folder: 'NOVATERIM/admin' },
-		function(error, result) {
-			console.log(result, error);
+	if(!resultMove){
+      const resultCloudinary = await cloudinary.uploader.upload(documentPath, { 
+			folder: `NOVATERIM/users/${username}`,
+			public_id: `${uniqid()}`
 		});
+		console.log(resultCloudinary);
+      res.json({ result: true, url: resultCloudinary.secure_url, name: resultCloudinary.original_filename });
+   }else{
+      res.json({ result: false, error: resultMove });
+   }
 
-		const fileUrl = result.secure_url;
-
-		// Saving the url to database
-
-		//
-
-		res.json({ result: true, url: fileUrl });
-
-	}catch (error) {
-		console.error('Cloudinary upload error', error);
-		res.json({ result: false, error: 'Error uploading file to Cloudinary' })
-	}
+   fs.unlinkSync(documentPath);
    
 });
 
