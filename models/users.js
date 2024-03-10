f278bbf84ef93ad5d121b5171a340e0ae98e44f2
JacquesOP <jacquesoprince.dev@gmail.com>
2024-03-10 23:24:32 +0100

    backend update

--- models/users.js
+++ models/users.js
@@ -1,55 +1,55 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const parametersSchema = mongoose.Schema({
-  email: Boolean,
-  notification: Boolean,
-  sms: Boolean,
-  importantNotifications: Boolean,
-  informations: Boolean,
-})
+   email: Boolean,
+   notification: Boolean,
+   sms: Boolean,
+   importantNotifications: Boolean,
+   informations: Boolean,
+});
 
 const salarySchema = mongoose.Schema({
-  dates: String,
-  salary: Number,
-  urlPdf: String,
-})
+   dates: String,
+   salary: Number,
+   urlPdf: String,
+});
 
 const contractSchema = mongoose.Schema({
-  title: String,
-  role: String,
-  description: String,
-  duration: String,
-  location: String,
-  workingType: String,
-  company: String,
-  dates: String,
-  salary: String,
-  status: String,
-  endDate: String,
-  urlPdf: String,
-  reference: String,
-  isSignatureReady: String,
-})
+   title: String,
+   role: String,
+   description: String,
+   duration: String,
+   location: String,
+   workingType: String,
+   company: String,
+   dates: String,
+   salary: String,
+   status: String,
+   endDate: String,
+   urlPdf: String,
+   reference: String,
+   isSignatureReady: String,
+});
 
 const identitySchema = mongoose.Schema({
-  name: String,
-  firstName: String,
-  birthDate: String,
-  nationality: String,
-  countryBirth: String,
-  familySituation: String,
-  birthTown: String,
-  birthDistrict: String,
-  socialSecurityNumber: String,
-  phoneNumber: Number,
-})
+   name: String,
+   firstName: String,
+   birthDate: String,
+   nationality: String,
+   countryBirth: String,
+   familySituation: String,
+   birthTown: String,
+   birthDistrict: String,
+   socialSecurityNumber: String,
+   phoneNumber: Number,
+});
 
 const addressSchema = mongoose.Schema({
-  street: String,
-  zipCode: Number,
-  city: String,
-  country: String,
-})
+   street: String,
+   zipCode: Number,
+   city: String,
+   country: String,
+});
 
 const userSchema = mongoose.Schema({
    email: String,
@@ -69,6 +69,6 @@ const userSchema = mongoose.Schema({
    homePaper: String,
 });
 
-const User = mongoose.model("users", userSchema)
+const User = mongoose.model("users", userSchema);
 
-module.exports = User
+module.exports = User;
